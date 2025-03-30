29fb450040e9a32c591838426f50e7451e27df32
Ryan Hawkins <rkhawkins80@hotmail.com>
2025-03-29 19:30:51 -0500

    updated 'displayMessage' function

--- js/helper.js
+++ js/helper.js
@@ -1,5 +1,13 @@
+import { currentPlayer } from "./monopoly.js";
+
 const messageDisplay = document.querySelector("#message-display")
 
-export function displayMessage(message) {
-    messageDisplay.textContent = message;
+export function displayMessage(...messages) {
+    console.log("messages", messages);
+    messageDisplay.innerHTML = `<p><i>${currentPlayer.name}</i></p>`;
+    for (let message of messages) {
+        messageDisplay.innerHTML += `<p>${message}</p>`;
+    }
+
+    // messageDisplay.textContent = message;
 }
